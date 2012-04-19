7e5ca273e0591890237bbc81d92e8b4a9642882c
Josh Deeden <jdeeden@gmail.com>
2012-04-19 05:10:13 -0700

    cleaning up spec formatting

--- spec/mortgage_spec.js
+++ spec/mortgage_spec.js
@@ -17,51 +17,39 @@ describe("Mortgage", function() {
       it("years", function() {
         expect(mortgage.years()).toEqual(defaults.years);
       });
-
       it("price", function() {
         expect(mortgage.price()).toEqual(100000);
       });
-
       it("market_value", function() {
         expect(mortgage.market_value()).toEqual(100000);
       });
       it("down_payment_percent", function() {
         expect(mortgage.down_payment_percent()).toEqual(10);
       });
-
-
       it("down_payment", function() {
         expect(mortgage.down_payment()).toEqual(10000);
       });
       it("amount", function() {
         expect(mortgage.amount()).toEqual(90000);
       });
-
       it("interest_percent", function() {
         expect(mortgage.interest_percent()).toEqual(6.5);
       });
-
-
-
       it("annual_taxes", function() {
         expect(mortgage.annual_taxes()).toEqual(500);
       });
-
       it("annual_insurance", function() {
         expect(mortgage.annual_insurance()).toEqual(500);
       });
-
       it("monthly_payment", function() {
         expect(mortgage.monthly_payment()).toEqual(568.86);
       });
       it("annual_payment", function() {
         expect(mortgage.annual_payment()).toEqual(6826.32);
       });
-
       it("monthly_pmi", function() {
         expect(mortgage.monthly_pmi()).toEqual(49.50);
       });
-
       it("ltv_ratio_percent", function() {
         expect(mortgage.ltv_ratio_percent()).toEqual(90);
       });
@@ -92,58 +80,45 @@ describe("Mortgage", function() {
       it("years", function() {
         expect(mortgage.years()).toEqual(defaults.years);
       });
-
       it("price", function() {
         expect(mortgage.price()).toEqual(250000);
       });
-
       it("market_value", function() {
         expect(mortgage.market_value()).toEqual(250000);
       });
       it("down_payment_percent", function() {
         expect(mortgage.down_payment_percent()).toEqual(20);
       });
-
-
       it("down_payment", function() {
         expect(mortgage.down_payment()).toEqual(50000);
       });
       it("amount", function() {
         expect(mortgage.amount()).toEqual(200000);
       });
-
       it("interest_percent", function() {
         expect(mortgage.interest_percent()).toEqual(6.5);
       });
-
-
-
       it("annual_taxes", function() {
         expect(mortgage.annual_taxes()).toEqual(1000);
       });
-
       it("annual_insurance", function() {
         expect(mortgage.annual_insurance()).toEqual(1000);
       });
-
       it("monthly_payment", function() {
         expect(mortgage.monthly_payment()).toEqual(1264.14);
       });
       it("annual_payment", function() {
         expect(mortgage.annual_payment()).toEqual(15169.68);
       });
-
       it("monthly_pmi", function() {
         expect(mortgage.monthly_pmi()).toEqual(0);
       });
-
       it("ltv_ratio_percent", function() {
         expect(mortgage.ltv_ratio_percent()).toEqual(80);
       });
       it("annual_pmi", function() {
         expect(mortgage.annual_pmi()).toEqual(0);
       });
-
       it("amortization table", function() {
         expect(mortgage.amortization_table() !== undefined);
       });
@@ -168,64 +143,49 @@ describe("Mortgage", function() {
       it("years", function() {
         expect(mortgage.years()).toEqual(defaults.years);
       });
-
       it("price", function() {
         expect(mortgage.price()).toEqual(1000000);
       });
-
       it("market_value", function() {
         expect(mortgage.market_value()).toEqual(1000000);
       });
       it("down_payment_percent", function() {
         expect(mortgage.down_payment_percent()).toEqual(40);
       });
-
-
       it("down_payment", function() {
         expect(mortgage.down_payment()).toEqual(400000);
       });
       it("amount", function() {
         expect(mortgage.amount()).toEqual(600000);
       });
-
       it("interest_percent", function() {
         expect(mortgage.interest_percent()).toEqual(5.5);
       });
-
-
-
       it("annual_taxes", function() {
         expect(mortgage.annual_taxes()).toEqual(10000);
       });
-
       it("annual_insurance", function() {
         expect(mortgage.annual_insurance()).toEqual(10000);
       });
-
       it("monthly_payment", function() {
         expect(mortgage.monthly_payment()).toEqual(3406.73);
       });
       it("annual_payment", function() {
         expect(mortgage.annual_payment()).toEqual(40880.76);
       });
-
       it("monthly_pmi", function() {
         expect(mortgage.monthly_pmi()).toEqual(0);
       });
-
       it("ltv_ratio_percent", function() {
         expect(mortgage.ltv_ratio_percent()).toEqual(60);
       });
       it("annual_pmi", function() {
         expect(mortgage.annual_pmi()).toEqual(0);
       });
-
       it("amortization table", function() {
         expect(mortgage.amortization_table() !== undefined);
       });
     });
   });
-
-
 });
 
